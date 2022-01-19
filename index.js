72f5f89593d43620943caa899074cc220bfca8e2
Fpiton32 <98000957+Fpiton32@users.noreply.github.com>
2022-01-19 00:19:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end(&quot;Hello World VM UP!");
+    response.end(&quot;Hello World VM UP + Falha!");
 });
 
 const port = process.env.PORT || 1337;
